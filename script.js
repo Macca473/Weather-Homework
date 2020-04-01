b97d32db6c9fc6b037e19dc2d36ebc7924e2620c
Matthew <mmacca473@gmail.com>
2020-04-01 17:24:47 +1030

    begin on the weather info

--- script.js
+++ script.js
@@ -1,28 +1,18 @@
 console.log("test")
 
-var Loc = "Adelaide";
-
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + Loc + ",au&APPID=1566020c66651f6712fd865503112dec";
 
 var locarr = [];
+
+
 
 locstrfls = localStorage.getItem("Location array");
 
 for (var locs = 0; locs < locstrfls.length; locs++) {
     
     locstrfls = locstrfls.replace(",","");
-
 }
     
 $( ".locns" ).append(locstrfls);
-
-// var locs
-// for (locs = 0; locs < locarr.length; locs++) {
-   
-    
-//     console.log(locarr[locs]);
-// }
-
 
 $("#submit").click(function() {
     $(".form-control").html(function() {
@@ -31,24 +21,55 @@ $("#submit").click(function() {
         $( ".locns" ).append("<div>" + $(this).val() + "</div>");
         // console.log(locarr);
         localStorage.setItem("Location array",locarr);
-        
-
-        console.log("array " + locarr);
-        // localStorage.setItem("Location array", locarr);
-
     })
-  })
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function ajx(response) {
-    // console.log(response);
+        // console.log($(".form-control").val());
+
+        var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + $(".form-control").val() + "&APPID=1566020c66651f6712fd865503112dec";
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function ajx(response) {
+            console.log(response.list[0].dt_txt);
+
+            $("#locdate").html(
+                    $(".form-control").val() + " " +
+                    response.list[0].dt_txt.substr(8, 2) + "/" +
+                    response.list[0].dt_txt.substr(5, 2) + "/" +
+                    response.list[0].dt_txt.substr(0, 4)
+            );
+
+            // console.log(response.list[1].weather[0].icon);
+
+            // console.log(response.list[1].weather[0].main);
+
+            // console.log("<img alt=" + response.list[1].weather[0].main + " src=./Images/" + response.list[1].weather[0].icon + "@2x.png width=50 height=50>");
+
+            $("#wthicn").html(
+                    "<img alt=" +
+                    response.list[1].weather[0].main +
+                    " src=./Images/" + response.list[1].weather[0].icon +
+                    "@2x.png width=50 height=50>"
+            );
+
+            // 273.15
+
+            // console.log(response.list[1].main.temp)
+
+            function ftc() {
+                var temp = response.list[1].main.temp - 273.15;
+                console.log(temp);
+                return temp;
+            }
+
+            $("#temp").html(
+                "temperature: " + ftc()
+            );
     
-});
+        });
 
-// var locs
-// for (locs = 0; locs < locarr.length; locs++) {
-//     console.log(locarr[locs]);
-//     $( ".locns" ).append(locarr[locs]);
-// }
+        })
+
+//////////////////////////////////////////////////////////////////////
+
